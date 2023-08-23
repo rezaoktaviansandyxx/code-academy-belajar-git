d5f50e8179e678cebf365fc359cd1a7bea41bcc2
indra <indraokisandy1@gmail.com>
2023-08-23 14:01:35 +0700

    Belajar Javascript

---
+++ index.js
@@ -0,0 +1,31 @@
+// let number = 10;
+// number = 5;
+// // console.log(number);
+
+// const pi = 3.14;
+// // pi = 3;
+// console.log(pi);
+
+// let num1 = 10;
+// let num2 = "abc";
+
+// var tambah = num1 + num2;
+// var kurang = num1 - num2;
+// console.log(total);
+
+// let x = 10;
+// let y = 5;
+
+// const result =
+//   x > y
+//     ? "x lebih besar dari y"
+//     : x < y
+//     ? "x lebih kecil dari y"
+//     : "x sama dengan y";
+
+// console.log(result);
+
+// function hello() {
+//   console.log("Hello");
+// }
+// console.log(hello());
